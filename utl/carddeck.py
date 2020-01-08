b24bcce780242c93b5ce34fdbf4385a300538aba
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2020-01-08 08:57:47 -0500

    utility for using urllib3 cleanly

--- utl/carddeck.py
+++ utl/carddeck.py
@@ -5,13 +5,19 @@
 
 import urllib3, json
 
-NEWDECK = 'https://deckofcardsapi.com/api/deck/new/shuffle/'
-DRAWCARDS = 'https://deckofcardsapi.com/api/deck/{}/draw/'
+#====================================================
+# URLLIB3 UTILITIES
 
 class RequestException(Exception):
     ''' class RequestException: exception to be raised when the Deck of Cards API receives a status other than success'''
     pass
 
+def http_getJSON( url, method = 'GET', args = {} ):
+    ''' http_getJSON(): use urllib3 to issue a request to a given url and convert its response from JSON into a dictionary '''
+    http = urllib3.PoolManager()
+    response = http.request(method,url)
+    return json.loads(response.data.decode('utf-8'))
 
 
-
+NEWDECK = 'https://deckofcardsapi.com/api/deck/new/shuffle/'
+DRAWCARDS = 'https://deckofcardsapi.com/api/deck/{}/draw/'
