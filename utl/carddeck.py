77e62f51f1e6fddcd2e940e67741ac26e654ad63
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2020-01-08 09:05:31 -0500

    deck of cards api function calls

--- utl/carddeck.py
+++ utl/carddeck.py
@@ -18,6 +18,32 @@ def http_getJSON( url, method = 'GET', args = {} ):
     response = http.request(method,url)
     return json.loads(response.data.decode('utf-8'))
 
+#====================================================
+# DECK OF CARDS API FUNCTION CALLS
 
 NEWDECK = 'https://deckofcardsapi.com/api/deck/new/shuffle/'
 DRAWCARDS = 'https://deckofcardsapi.com/api/deck/{}/draw/'
+
+def newdeck( args = {} ):
+    ''' newdeck(): calls deck of cards API and creates a new deck
+        RETURN VALUE: id string returned to represent deck '''
+    data = http_getJSON(NEWDECK,args = args)
+    if data and data['success']:
+        return data['deck_id']
+
+    print(data)
+    raise RequestException("Deck of Cards newdeck: see console")
+
+def drawcards(deck_id,n):
+    ''' drawcards(): calls deck of cards API and draws n cards from the specified deck id
+        RETURN VALUE: array of json-like dictionaries representing cards (see DoC docs) '''
+    data = http_getJSON(
+        DRAWCARDS.format(deck_id),
+        args = {'count':n}
+    )
+    if data and data['success']:
+        return data['cards']
+
+    print(data)
+    raise RequestException("Deck of Cards draw: see console")
+    
