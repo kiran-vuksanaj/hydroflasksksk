4c2bcfc49db021498b8f382c093e3c8da9d64836
Tiffany Cao <tcao00@stuy.edu>
2020-01-15 22:53:04 -0500

    fixed cooldown time in wheel

--- utl/db_manager.py
+++ utl/db_manager.py
@@ -212,17 +212,21 @@ def updateTime(username):
     q="SELECT time FROM user_tbl WHERE username=?"
     inputs=(username,)
     time=execmany(q,inputs).fetchone()[0]
+    print("HEREHEREHEREHEREHERE")
+    print(time)
     if(time=="0"):
         q="UPDATE user_tbl SET time=? WHERE username=?"
         now=datetime.now()+timedelta(days=1)
-        now=str(now.strftime("%m-%d-%Y %H:%M:%S"))
+        now=str(now.strftime("%m/%d/%Y %H:%M:%S"))
         inputs=(now,username)
         execmany(q,inputs)
+        print(now)
         return now
     time=time.split(" ")
     time[1]=str(time[1]).split(":")
-    time[0]=str(time[0]).split("/")
-    print(time[1])
+    time[0]=str(time[0]).split("-")
+    # print(time[1])
+    # print(time[0])
     prev=datetime(int(time[0][2]),int(time[0][0]),int(time[0][1]),int(time[1][0]),int(time[1][1]),int(time[1][2]))
     now=datetime.now()
     if(now>prev):
@@ -231,6 +235,7 @@ def updateTime(username):
         now=str(now.strftime("%m/%d/%Y %H:%M:%S"))
         inputs=(now,username)
         execmany(q,inputs)
+        print(now)
         return now
     else:
         return 'NONE'
@@ -242,7 +247,7 @@ def getTime(username):
     time=execmany(q,inputs).fetchone()[0]
     time=time.split(" ")
     time[1]=str(time[1]).split(":")
-    time[0]=str(time[0]).split("/")
+    time[0]=str(time[0]).split("-")
     prev=datetime(int(time[0][2]),int(time[0][0]),int(time[0][1]),int(time[1][0]),int(time[1][1]),int(time[1][2]))
     prev=str(prev.strftime("%m/%d/%Y %H:%M:%S"))
     return prev
